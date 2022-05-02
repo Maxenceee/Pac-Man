030f819cd76baa17e4753d2ce589f3dfe2b44b45
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2022-05-02 14:05:15 +0200

    Initial Commit

---
+++ pacman/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pacman
+//
+//  Created by Maxence Gama on 02/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
